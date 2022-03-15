bd89c1c099d2603b4531076abe49db0166324968
Satwik Chappidi <satwik.chappidi@iitkgp.ac.in>
2022-03-15 19:16:03 +0530

    launch_script, src added; drone takes off

---
+++ src/path_planning/graph_planner.py
@@ -0,0 +1,217 @@
+#!/usr/bin/python
+import rospy
+from geometry_msgs.msg import PointStamped, PoseStamped, Quaternion, Point, Pose
+from std_msgs.msg import Bool
+import numpy as np
+import tf.transformations
+from scipy.interpolate import UnivariateSpline
+import math
+
+# Constants
+
+uav_height = 18.0
+PI = 3.1415
+reach_threshold = 0.05
+new_wp_resolution = 0.5
+deque_size = 4
+
+class Node():
+    def __init__(self, uav, ugv, index):
+        self.UGV = ugv
+        self.UAV = uav
+
+        self.parent = -1
+        self.index = index
+
+        self.children = []
+
+class Graph():
+    def __init__(self):
+        self.arr = []
+        self.last_nodes = [0]
+        self.root = 0
+        self.current = 0
+
+def getYawFromSpline():
+    t = np.arange(deque_size)
+
+    np_x = np.array([graph.arr[i].UAV.position.x for i in graph.last_nodes])
+    np_y = np.array([graph.arr[i].UAV.position.y for i in graph.last_nodes])
+
+    spl_x = UnivariateSpline(t, np_x)
+    spl_y = UnivariateSpline(t, np_y)
+
+    point = deque_size-1
+    tan = spl_y.derivative()(point)/spl_x.derivative()(point)
+
+    atan = np.arctan(tan)
+    other_yaw = 0.0
+
+    if (atan > 0):
+        other_yaw = atan - np.pi
+    elif (atan < 0):
+        other_yaw = atan + np.pi
+
+    quaternion = (current_uav_pose.pose.orientation.x,
+    current_uav_pose.pose.orientation.y,
+    current_uav_pose.pose.orientation.z,
+    current_uav_pose.pose.orientation.w)
+    euler = tf.transformations.euler_from_quaternion(quaternion)
+
+    current_yaw = euler[2]
+
+
+    yaw = 0
+    if(abs(current_yaw - atan) < abs(current_yaw - other_yaw)):
+        yaw = atan
+    else:
+        yaw = other_yaw
+
+    quaternion = tf.transformations.quaternion_from_euler(0, 0, yaw)
+
+    return quaternion
+
+
+def euler_from_quaternion(x, y, z, w):
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return roll_x, pitch_y, yaw_z # in radians
+
+def gps_point_forward(data):
+    pose_to_wp_angle = math.atan2(data.point.y - current_uav_pose.pose.position.y, data.point.x - current_uav_pose.pose.position.x)
+    _, __, y = euler_from_quaternion(current_uav_pose.pose.orientation.x, current_uav_pose.pose.orientation.y, current_uav_pose.pose.orientation.z,current_uav_pose.pose.orientation.w)
+
+    for theta in [pose_to_wp_angle, y]:
+        if(theta < 0):
+            theta += 2 * np.pi
+    if(abs(y - pose_to_wp_angle) < np.pi / 2):
+        return True
+    elif(2 * np.pi - abs(y - pose_to_wp_angle) < np.pi / 2):
+        return True
+    else:
+        return False
+
+
+def center_GPS_cb(data):
+    print("Received GPS point: ({0}, {1}, {2})".format(data.point.x, data.point.y, data.point.z))
+
+    if(gps_point_forward(data)):
+        arr_1 = np.array([data.point.x, data.point.y])
+        arr_2 = np.array([graph.arr[graph.current].UAV.position.x, graph.arr[graph.current].UAV.position.y ])
+
+        dist = np.linalg.norm(arr_1 - arr_2)
+        print(dist)
+        print(arr_1)
+        print(arr_2)
+        if (dist > new_wp_resolution):
+            uav = Pose()    
+            ugv = Pose()
+
+            uav.position.x = data.point.x
+            uav.position.y = data.point.y
+            uav.position.z = data.point.z + 18.0
+
+            ugv.position.x = data.point.x
+            ugv.position.y = data.point.y
+            ugv.position.z = data.point.z
+
+            temp = Node(uav, ugv, len(graph.arr))
+            graph.arr.append(temp)
+            temp.parent = graph.current
+            graph.arr[graph.current].children.append(temp.index)
+            graph.current = temp.index
+
+            graph.last_nodes.append(graph.current)
+            if (len(graph.last_nodes) > deque_size):
+                while (len(graph.last_nodes) != deque_size):
+                    graph.last_nodes.pop(0)
+
+            if (len(graph.last_nodes) == deque_size):
+                quat = getYawFromSpline()
+                graph.arr[graph.current].UAV.orientation.x = quat[0]
+                graph.arr[graph.current].UAV.orientation.y = quat[1]
+                graph.arr[graph.current].UAV.orientation.z = quat[2]
+                graph.arr[graph.current].UAV.orientation.w = quat[3]
+                graph.arr[graph.current].UGV.orientation.x = quat[0]
+                graph.arr[graph.current].UGV.orientation.y = quat[1]
+                graph.arr[graph.current].UGV.orientation.z = quat[2]
+                graph.arr[graph.current].UGV.orientation.w = quat[3]
+            
+            print("Added Node point: ({0}, {1}, {2})".format(graph.arr[graph.current].UAV.position.x, graph.arr[graph.current].UAV.position.y, graph.arr[graph.current].UAV.position.z))
+            uav_wp.pose = graph.arr[graph.current].UAV
+            uav_wp_pub.publish(uav_wp)
+            get_GPS_flag.data = False
+            get_GPS_pub.publish(get_GPS_flag)
+
+def current_uav_pose_cb(data):
+    global current_uav_pose
+    current_uav_pose = data
+
+    # Reached Waypoint
+    arr_1 = np.array([uav_wp.pose.position.x, uav_wp.pose.position.y])
+    arr_2 = np.array([current_uav_pose.pose.position.x, current_uav_pose.pose.position.y])
+
+    dist = np.linalg.norm(arr_1 - arr_2)
+    if (dist < reach_threshold):
+        get_GPS_flag.data = True
+        get_GPS_pub.publish(get_GPS_flag)
+
+    arr_1 = np.array([end_ugv.position.x, end_ugv.position.y])
+    dist = np.linalg.norm(arr_1 - arr_2)
+    if (dist < reach_threshold):
+        end_reached_flag.data = True
+        end_reached_mapping_pub.publish(end_reached_flag)
+
+
+
+
+def graph_planner():
+    rospy.init_node('graph_planner')
+
+    global uav_wp_pub, get_GPS_pub, end_reached_mapping_pub
+    uav_wp_pub = rospy.Publisher("/uav_wp", PoseStamped, queue_size = 100)
+    get_GPS_pub = rospy.Publisher("/get_GPS", Bool, queue_size = 100)
+    end_reached_mapping_pub = rospy.Publisher("/end_reached_mapping", Bool, queue_size = 100)
+
+
+    global get_GPS_flag, end_reached_flag
+    get_GPS_flag = Bool()
+    get_GPS_flag.data = True
+    end_reached_flag = Bool()
+    end_reached_flag.data = False
+
+    center_GPS_sub = rospy.Subscriber("/lane_center", PointStamped, center_GPS_cb)
+    current_uav_pose_sub = rospy.Subscriber("/mavros/local_position/pose", PoseStamped, current_uav_pose_cb)
+
+    global graph
+    graph = Graph()
+
+    global uav_wp
+    uav_wp = PoseStamped()
+
+    global start_uav, start_ugv
+    start_uav = Pose()
+    start_ugv = Pose()
+    node = Node(start_uav, start_ugv, len(graph.arr))
+    graph.arr.append(node)
+
+    global end_ugv
+    end_ugv = Pose()
+
+    while not rospy.is_shutdown():
+        rospy.spin()
+
+
+if __name__ == "__main__":
+    graph_planner()
---
+++ src/path_planning/init_drone.cpp
@@ -0,0 +1,188 @@
+//initially include all header files necessary for compiling the subscribers, publishers and services 
+//which are going to be used in the code
+
+ #include <ros/ros.h>
+
+ #include <geometry_msgs/PoseStamped.h>
+ #include <std_msgs/Bool.h>
+ #include <mavros_msgs/CommandBool.h>
+ #include <mavros_msgs/SetMode.h>
+//  #include <mavros_msgs/StreamRate.h>
+ #include <mavros_msgs/State.h>
+
+ #include <mavros_msgs/CommandLong.h>
+ #include <mavros_msgs/CommandTOL.h>
+ #include <mavros_msgs/PositionTarget.h>
+
+ //creating a callback 'state_cb' to store the current state of the copter in variable 'current_state' 
+ //this will be called by subscriber 'state_sub' when a message arrives in the 'mavros/state' topic 
+//which is of data type 'mavros_msgs/State'
+
+mavros_msgs::State current_state;              //this sets the variable type for 'current_state' 
+                                                                          //variable
+
+geometry_msgs::PoseStamped current_pose;
+geometry_msgs::PoseStamped initial_wp;
+std_msgs::Bool took_off;
+
+ros::Publisher local_pos_pub;
+ros::Publisher took_off_pub;
+
+double takeoff_height = 18.0;
+double reach_threshold = 0.5;
+
+void state_cb(const mavros_msgs::State::ConstPtr& msg){
+    current_state = *msg;
+}
+
+void wp_cb (const geometry_msgs::PoseStamped::ConstPtr& msg){
+    if (ros::ok() && current_state.armed)
+    {
+        ROS_INFO("Setting new waypoint..\nX: %f, Y: %f, Z: %f\n", msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);
+        local_pos_pub.publish(*msg);
+    }
+}
+
+void current_pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg)
+{
+    current_pose = *msg;
+    ROS_INFO("Current Pose:\nX: %f, Y: %f, Z: %f\n", msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);
+    if (took_off.data == false)
+    {
+        ROS_INFO("Distance to Takeoff Height: %f\n", abs(takeoff_height - current_pose.pose.position.z));
+        if (abs(takeoff_height - current_pose.pose.position.z) < reach_threshold)
+        {
+            took_off.data = true;
+            ROS_INFO("Reached altitude of %f\n", current_pose.pose.position.z);
+        }
+    }
+    took_off_pub.publish(took_off);
+}
+
+bool reached_wp(geometry_msgs::PoseStamped wp)
+{
+    // if (initial_wp.pose.orientation.x == -1)
+    //     return false;
+
+    // double diff_x = current_pose.pose.position.x - wp.pose.position.x;
+    // double diff_y = current_pose.pose.position.y - wp.pose.position.y;
+    double diff_z = current_pose.pose.position.z - takeoff_height;
+
+    // double dist = sqrt(pow(diff_x, 2) + pow(diff_y, 2) + pow(diff_z, 2));
+
+    return (diff_z < reach_threshold);
+}
+
+//the main ros function with all subscribers, publishers and services
+
+int main(int argc, char **argv)
+{
+    // initial_wp.pose.orientation.x = -1;
+
+    ros::init(argc, argv, "init_drone");  //initialising ros, with node name 'offn_node'
+    ros::NodeHandle nh;                  //main accesspoint for this node to communicate with the system 
+
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>      //subscriber 'state_sub' is subscribing to topic 'mavros/state of type mavros_msgs/State by calling state_cb
+     ("mavros/state", 10, state_cb);
+    ros::Subscriber current_pose_sub = nh.subscribe<geometry_msgs::PoseStamped>
+     ("mavros/local_position/pose", 10, current_pose_cb);
+    local_pos_pub = nh.advertise<geometry_msgs::PoseStamped> //publisher 'local_pos_pub' topic 'maros/.../local' type 'geo../PoseStamped'
+     ("mavros/setpoint_position/local", 10);
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>  //serviceclient 'arming_client' type(header) 'mav../Com..Bool' service 'mavros/cmd/arming'
+     ("mavros/cmd/arming");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>   //serviceclient 'set_mode_client' type(header) 'mav../SetMode' service 'mavros/set_mode'
+     ("mavros/set_mode");
+    ros::ServiceClient takeoff_cl = nh.serviceClient<mavros_msgs::CommandTOL> 
+     ("/mavros/cmd/takeoff");
+
+    ros::Subscriber waypoint_sub = nh.subscribe<geometry_msgs::PoseStamped>
+     ("/uav_wp", 10, wp_cb);
+
+    took_off_pub = nh.advertise<std_msgs::Bool>
+     ("/took_off", 10);
+    took_off.data = false;
+    
+
+    ros::Rate rate(20.0);                               //here we give 20Hz
+
+    ROS_INFO("Waiting for Connection...");
+    while(ros::ok() && !current_state.connected){
+        // ROS_INFO("%d", current_state.connected);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    ROS_INFO("Connection with FCU has been establised.");
+
+    mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "GUIDED";    //OFFBOARD node since we are 
+                                                                                                        //using px4 flight stack
+    offb_set_mode.request.base_mode = 0.0;
+
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;                       //true=> armig the copter
+    mavros_msgs::CommandBool disarm_cmd;
+    disarm_cmd.request.value = false;                   //fals=> disarmig the copter
+
+    ros::Time last_request = ros::Time::now();
+ 
+    while(ros::ok() && !current_state.armed){
+
+        if( current_state.mode != "GUIDED" &&
+            (ros::Time::now() - last_request > ros::Duration(5.0))){
+            if( set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.mode_sent){
+                ROS_INFO("GUIDED enabled");
+            }
+            last_request = ros::Time::now();
+
+        } else {
+            if( !current_state.armed &&
+                (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success){
+                    ROS_INFO("Vehicle Armed");
+                }
+                last_request = ros::Time::now();
+            }
+        }
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    last_request = ros::Time::now();
+    
+    mavros_msgs::CommandTOL srv_takeoff;
+    srv_takeoff.request.altitude = takeoff_height;
+
+    if(takeoff_cl.call(srv_takeoff)){
+        ROS_INFO("Takeoff sent %d", srv_takeoff.response.success);
+    }
+    else{
+        ROS_ERROR("Failed Takeoff");
+        return -1;
+    }
+
+   //after 5sec giving command to drone to takeoff to an altitude of 1.5m
+    // ROS_INFO("Initial Set to X: %f, Y: %f, Z: %f\n", initial_wp.pose.orientation.x, initial_wp.pose.position.y, initial_wp.pose.position.z);
+
+
+    // while(ros::ok() && current_state.armed){
+
+    //     if( ros::Time::now() - last_request > ros::Duration(1.0)){
+    //         // ROS_INFO("Initiating Takeoff");  
+            
+            
+    //         last_request = ros::Time::now();
+    //         // if (initial_wp.pose.orientation.x != -1)
+    //         //     local_pos_pub.publish(initial_wp);
+    //         ros::spinOnce();
+    //         rate.sleep();
+    //     }
+    // }
+
+    ros::spin();
+
+
+}
