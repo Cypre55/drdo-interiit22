270cb7fcde8936bafec3390ecd2dadebf368497f
Rohit-Dhamija <rohit77.rd@gmail.com>
2022-03-18 01:01:29 +0530

    added car and drone respawner(they exit automatically when controller is on)

---
+++ src/Controller/car_respawner.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+import rospy
+from geometry_msgs.msg import PointStamped, PoseStamped, Point
+
+import rospkg 
+from gazebo_msgs.msg import ModelState 
+from gazebo_msgs.srv import SetModelState
+
+from tf.transformations import quaternion_from_euler 
+
+
+from prius_msgs.msg import Control   
+
+
+flag = 0
+def check_if_controller_up(car_inputs):
+    global flag
+    if car_inputs.shift_gears != 1:
+        print("Controller_is_on")
+        flag = 1   
+
+
+def main():
+    
+    rospy.init_node('set_pose')
+
+    rospy.Subscriber('prius' , Control, check_if_controller_up) 
+
+
+    instance = rospy.Publisher('prius', Control, queue_size=10)
+
+    rate = rospy.Rate(10)
+
+    msg = Control()
+
+
+  
+    state_msg = ModelState()
+
+    state_msg = ModelState()
+    state_msg.model_name = 'prius'
+    state_msg.pose.position.x = -12.247099
+    state_msg.pose.position.y = 308.947301
+    state_msg.pose.position.z = 22.290122
+
+    roll,pitch,yaw = -0.005833,0.135839,-2.287164
+
+    quarternions = quaternion_from_euler(roll,pitch,yaw)
+
+    state_msg.pose.orientation.x = quarternions[0]
+    state_msg.pose.orientation.y = quarternions[1]
+    state_msg.pose.orientation.z = quarternions[2]
+    state_msg.pose.orientation.w = quarternions[3]
+
+    rospy.wait_for_service('/gazebo/set_model_state')
+    try:
+        
+        set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+        resp = set_state( state_msg )
+
+    except rospy.ServiceException, e:
+        print "Service call failed: %s" % e
+
+    
+
+
+    print("spawned")
+    global flag
+    while flag == 0 and not rospy.is_shutdown():
+
+     
+        msg.throttle = 0                             
+        msg.brake = 1 
+        msg.steer = 0
+        msg.shift_gears =1
+
+        instance.publish(msg)
+
+        
+
+        rate.sleep()
+
+    
+    print("exiting")
+
+
+
+
+
+
+        
+    
+
+if __name__ == "__main__":
+    main()
+
